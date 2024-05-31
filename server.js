74fad98cb887e41e98c5058720291a954384c987
Vidhu Prakash T P <132804281+VidhuPrakash@users.noreply.github.com>
2024-05-31 20:17:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ dotenv.config();
 //databse config
 connectDB();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 //rest object
 const app = express();
 
